90905ce5445e5f2756f49cd6140bf38ea0e0f721
Karl Arthur <karl.crispen.arthur@gmail.com>
2021-01-09 15:45:51 -0700

    Add a list of tent types

--- app/src/main/java/com/munifrog/design/tetheredtenttriangulator/MainActivity.java
+++ app/src/main/java/com/munifrog/design/tetheredtenttriangulator/MainActivity.java
@@ -3,6 +3,8 @@ package com.munifrog.design.tetheredtenttriangulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spin = findViewById(R.id.sp_models);
+        String [] array = getResources().getStringArray(R.array.tent_models);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                R.layout.text_spinner,
+                array
+        );
+        arrayAdapter.setDropDownViewResource(R.layout.text_spinner);
+        spin.setAdapter(arrayAdapter);
     }
 }
