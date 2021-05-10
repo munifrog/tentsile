ab6a7cb50f81425e206cb7fb67ba555bc1098481
Karl Arthur <karl.crispen.arthur@gmail.com>
2021-05-09 22:42:05 -0600

    test: Add unit test for Util.getDirection

---
+++ app/src/test/java/com/munifrog/design/tetheredtenttriangulator/UtilUnitTests.java
@@ -0,0 +1,38 @@
+package com.munifrog.design.tetheredtenttriangulator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+// Note: cosine and sine are computed in radians, not degrees
+public class UtilUnitTests {
+    private final String TAG = getClass().getSimpleName();
+
+    @Test
+    public void getDirection_isWorking() {
+        double allowance = 0.001;
+        int iterations = 360;
+        double angleDiff = 2 * Math.PI / iterations;
+        double hypotenuse = 1.0;
+        System.out.println(TAG + "angleDiff: \"" + angleDiff + "\"");
+
+        double angle, negAngle, deltaX, deltaY, derivedAngle;
+        for (int i = 0; i < iterations; i++) {
+            // Positive angles
+            angle = i * angleDiff; // technically added to 0.0, the starting point
+            deltaX = hypotenuse * Math.cos(angle);
+            deltaY = hypotenuse * Math.sin(angle);
+            derivedAngle = Util.getDirection(hypotenuse, deltaX, deltaY);
+            if (derivedAngle < 0) { derivedAngle += 2 * Math.PI; }
+            assertEquals(angle, derivedAngle, allowance);
+
+            // Negative angles
+            negAngle = angle - 2 * Math.PI;
+            deltaX = hypotenuse * Math.cos(negAngle);
+            deltaY = hypotenuse * Math.sin(negAngle);
+            derivedAngle = Util.getDirection(hypotenuse, deltaX, deltaY);
+            if (derivedAngle < 0) { derivedAngle += 2 * Math.PI; }
+            assertEquals(angle, derivedAngle, allowance);
+        }
+    }
+}
