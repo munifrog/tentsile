5d388c544cba242ac89881477548b5b3b9fbe2f7
Karl Arthur <karl.crispen.arthur@gmail.com>
2021-05-15 14:22:29 -0600

    test: Add unit tests for computing tether-center

---
+++ app/src/test/java/com/munifrog/design/tetheredtenttriangulator/TetherCenterUnitTests.java
@@ -0,0 +1,40 @@
+package com.munifrog.design.tetheredtenttriangulator;
+
+import org.junit.Test;
+
+import static com.munifrog.design.tetheredtenttriangulator.UnitTestUtil.ALLOWANCE_DELTA_THREE;
+import static com.munifrog.design.tetheredtenttriangulator.UnitTestUtil.MATH_CENTER_X;
+import static com.munifrog.design.tetheredtenttriangulator.UnitTestUtil.MATH_CENTER_Y;
+import static com.munifrog.design.tetheredtenttriangulator.UnitTestUtil.TETHERS_EQUILATERAL;
+import static com.munifrog.design.tetheredtenttriangulator.UnitTestUtil.TETHERS_ISOSCELES;
+import static com.munifrog.design.tetheredtenttriangulator.UnitTestUtil.THRESHOLD_PLATFORM;
+import static org.junit.Assert.*;
+
+public class TetherCenterUnitTests {
+    @Test
+    public void PlatformCenterRun_equilateral_isWorking() {
+        TetherCenter platformCenter = new TetherCenter(TETHERS_EQUILATERAL, THRESHOLD_PLATFORM);
+        platformCenter.process();
+        float[] actual = platformCenter.getCenter();
+        float[] expected = { (float) MATH_CENTER_X, (float) MATH_CENTER_Y };
+        assertArrayEquals(expected, actual, (float)ALLOWANCE_DELTA_THREE);
+    }
+
+    @Test
+    public void PlatformCenterRun_isosceles_isWorking() {
+        TetherCenter platformCenter = new TetherCenter(TETHERS_ISOSCELES, THRESHOLD_PLATFORM);
+        platformCenter.process();
+        float[] actual = platformCenter.getCenter();
+        float[] expected = { (float) MATH_CENTER_X, (float) MATH_CENTER_Y };
+        assertArrayEquals(expected, actual, (float)ALLOWANCE_DELTA_THREE);
+    }
+
+    @Test
+    public void PlatformCenterRun_scalene_isWorking() {
+        TetherCenter platformCenter = new TetherCenter(TETHERS_ISOSCELES, THRESHOLD_PLATFORM);
+        platformCenter.process();
+        float[] actual = platformCenter.getCenter();
+        float[] expected = { (float) MATH_CENTER_X, (float) MATH_CENTER_Y };
+        assertArrayEquals(expected, actual, (float)ALLOWANCE_DELTA_THREE);
+    }
+}
