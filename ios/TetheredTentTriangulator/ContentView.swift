a8bdff3db152e6ddd5408e817edb1fe62208d171
Karl Arthur <karl.crispen.arthur@gmail.com>
2021-09-05 16:37:58 -0600

    chore: Initial iOS commit

---
+++ ios/TetheredTentTriangulator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TetheredTentTriangulator
+//
+//  Created by Karl Arthur on 9/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/TetheredTentTriangulator/TetheredTentTriangulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetheredTentTriangulatorApp.swift
+//  TetheredTentTriangulator
+//
+//  Created by Karl Arthur on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TetheredTentTriangulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
