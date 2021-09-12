8ddeb3802babe3fde1e7988471e5b0c3d0e8c3f6
Karl Arthur <karl.crispen.arthur@gmail.com>
2021-09-11 21:21:09 -0600

    feat: ios: Draw circle where touch occurs

--- ios/TetheredTentTriangulator/Clearing.swift
+++ ios/TetheredTentTriangulator/Clearing.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct Clearing: View {
+    @State var touchPoint = CGPoint(x:0, y:0)
+
     var body: some View {
         Rectangle()
             .aspectRatio(0.66667, contentMode: .fit)
             .foregroundColor(Color("Clearing"))
+            .gesture(
+                // https://stackoverflow.com/a/60219793
+                DragGesture(minimumDistance: 0)
+                    .onChanged({ (touch) in
+                        self.touchPoint = touch.location
+                    })
+                    .onEnded({ (touch) in
+                        self.touchPoint = touch.location
+                    })
+            )
+            .overlay(
+                Text("(\(Int(touchPoint.x)), \(Int(touchPoint.y)))")
+                    .font(.title)
+                    .colorInvert()
+                )
+            .overlay(
+                Circle()
+                    .fill(Color.green)
+                    .frame(width: 15, height: 15, alignment: .center)
+                    .position(touchPoint)
+                )
     }
 }
 
