ba9c6ec2e2e0374889c4dde5f005ad3d470f6c03
Karl Arthur <karl.crispen.arthur@gmail.com>
2021-09-19 17:23:26 -0600

    feat: ios: Allow moving entire triangle within bounds selecting tether center

--- ios/TetheredTentTriangulator/Configuration.swift
+++ ios/TetheredTentTriangulator/Configuration.swift
@@ -23,6 +23,10 @@ struct Configuration {
 
     private var selection: Select = .none
     private var radiusSquared: Float = 225
+    private var initial_p: Coordinate?
+    private var initial_a: Coordinate?
+    private var initial_b: Coordinate?
+    private var initial_c: Coordinate?
     private var limits: Coordinate
 
     init() {
@@ -30,6 +34,7 @@ struct Configuration {
         self.util = Util()
         self.center = util.getTetherCenter(self.anchors)
         self.limits = Coordinate(x: 0.0, y: 0.0)
+        self.resetInitialPositions()
     }
 
     init(anchors: Anchors) {
@@ -37,6 +42,7 @@ struct Configuration {
         self.util = Util()
         self.center = util.getTetherCenter(self.anchors)
         self.limits = Coordinate(x: 0.0, y: 0.0)
+        self.resetInitialPositions()
     }
 
     mutating func setLimits(screen: Coordinate) {
@@ -47,67 +53,172 @@ struct Configuration {
         return self.limits
     }
 
-    mutating func endSelection() {
-        self.selection = .none
+    mutating private func resetInitialPositions() {
+        self.initial_p = nil
+        self.initial_a = nil
+        self.initial_b = nil
+        self.initial_c = nil
     }
 
-    mutating func updateSelection(coordinate: Coordinate) {
+    mutating private func saveInitialPositions() {
+        self.initial_p = self.center?.p
+        self.initial_a = self.anchors.a
+        self.initial_b = self.anchors.b
+        self.initial_c = self.anchors.c
+    }
+
+    mutating func endSelection() {
+        self.selection = .none
+        self.resetInitialPositions()
+    }
+
+    mutating func updateSelection(touch: Coordinate) {
         // When already selected, allow the selected point to be updated with the new location
         switch selection {
         case .anchor_a:
-            self.anchors.a.x = coordinate.x
-            self.anchors.a.y = coordinate.y
-            self.center = util.getTetherCenter(self.anchors)
+            anchors.a = getBoundedTouch(touch: touch)
+            center = util.getTetherCenter(self.anchors)
         case .anchor_b:
-            self.anchors.b.x = coordinate.x
-            self.anchors.b.y = coordinate.y
-            self.center = util.getTetherCenter(self.anchors)
+            anchors.b = getBoundedTouch(touch: touch)
+            center = util.getTetherCenter(self.anchors)
         case .anchor_c:
-            self.anchors.c.x = coordinate.x
-            self.anchors.c.y = coordinate.y
-            self.center = util.getTetherCenter(self.anchors)
+            anchors.c = getBoundedTouch(touch: touch)
+            center = util.getTetherCenter(self.anchors)
         case .missed:
             // Do nothing
             break
         case .none:
             // Assume the user did not touch close enough to any anchor point
             // Set selection as missed to ignore any other motion events
-            selection = .missed
-
-            // Determine if any selection points are close enough to the touch point
-            var closestDist = radiusSquared
-            var newSelection: Select = selection
-
-            var diff_x = coordinate.x - self.anchors.a.x
-            var diff_y = coordinate.y - self.anchors.a.y
-            var diffSquared = diff_x * diff_x + diff_y * diff_y
-            if diffSquared < closestDist {
-                newSelection = .anchor_a
-                closestDist = diffSquared
-            }
-
-            diff_x = coordinate.x - self.anchors.b.x
-            diff_y = coordinate.y - self.anchors.b.y
-            diffSquared = diff_x * diff_x + diff_y * diff_y
-            if diffSquared < closestDist {
-                newSelection = .anchor_b
-                closestDist = diffSquared
-            }
-
-            diff_x = coordinate.x - self.anchors.c.x
-            diff_y = coordinate.y - self.anchors.c.y
-            diffSquared = diff_x * diff_x + diff_y * diff_y
-            if diffSquared < closestDist {
-                newSelection = .anchor_c
-                closestDist = diffSquared
-            }
-
-            // TODO: Add another comparison here when there is a tether center
-
-            selection = newSelection
+            self.selection = .missed
+            self.selection = self.getSelection(touch: touch)
         case .point:
-            // Update all of the anchor points by the same vector
+            self.updateAnchors(touch: touch)
             break
         }
+    }
+
+    func getBoundedTouch(touch: Coordinate) -> Coordinate {
+        var limitedV = Coordinate(x: touch.x, y: touch.y)
+        let max = self.limits
+        let min = -max
+
+        if touch.x < min.x {
+            limitedV.x = min.x
+        } else if touch.x > max.x {
+            limitedV.x = max.x
+        }
+        if touch.y < min.y {
+            limitedV.y = min.y
+        } else if touch.y > max.y {
+            limitedV.y = max.y
+        }
+        return limitedV
+    }
+
+    mutating func getSelection(touch: Coordinate) -> Select {
+        // Determine if any selection points are close enough to the touch point
+        var closestDist = self.radiusSquared
+        var newSelection: Select = self.selection
+
+        var diff = touch - self.anchors.a
+        var diffSquared = diff.x * diff.x + diff.y * diff.y
+        if diffSquared < closestDist {
+            newSelection = .anchor_a
+            closestDist = diffSquared
+        }
+
+        diff = touch - self.anchors.b
+        diffSquared = diff.x * diff.x + diff.y * diff.y
+        if diffSquared < closestDist {
+            newSelection = .anchor_b
+            closestDist = diffSquared
+        }
+
+        diff = touch - self.anchors.c
+        diffSquared = diff.x * diff.x + diff.y * diff.y
+        if diffSquared < closestDist {
+            newSelection = .anchor_c
+            closestDist = diffSquared
+        }
+
+        if let center = self.center {
+            diff = touch - center.p
+            diffSquared = diff.x * diff.x + diff.y * diff.y
+            if diffSquared < closestDist {
+                newSelection = .point
+                //closestDist = diffSquared
+                saveInitialPositions()
+            }
+        }
+
+        return newSelection
+    }
+
+    mutating func updateAnchors(touch: Coordinate) {
+        // Update all of the anchor points by the same vector
+        guard let initP = self.initial_p else { return }
+        guard let initA = self.initial_a else { return }
+        guard let initB = self.initial_b else { return }
+        guard let initC = self.initial_c else { return }
+
+        // touch = initP + v
+        let v = touch - initP
+
+        // Limit the anchors to within the Clearing view
+        let tempA = initA + v
+        let tempB = initB + v
+        let tempC = initC + v
+
+        // Need to determine the greatest value in each direction
+        var smallestX = tempA.x
+        if (tempB.x < smallestX) {
+            smallestX = tempB.x
+        }
+        if (tempC.x < smallestX) {
+            smallestX = tempC.x
+        }
+        var largestX = tempA.x
+        if (tempB.x > largestX) {
+            largestX = tempB.x
+        }
+        if (tempC.x > largestX) {
+            largestX = tempC.x
+        }
+
+        var smallestY = tempA.y
+        if (tempB.y < smallestY) {
+            smallestY = tempB.y
+        }
+        if (tempC.y < smallestY) {
+            smallestY = tempC.y
+        }
+        var largestY = tempA.y
+        if (tempB.y > largestY) {
+            largestY = tempB.y
+        }
+        if (tempC.y > largestY) {
+            largestY = tempC.y
+        }
+
+        var limitedV = Coordinate(x: 0.0, y: 0.0)
+        let max = self.limits
+        let min = -max
+
+        if smallestX < min.x {
+            limitedV.x =  (min.x - smallestX)
+        } else if largestX > max.x {
+            limitedV.x =  (max.x - largestX)
+        }
+        if smallestY < min.y {
+            limitedV.y =  (min.y - smallestY)
+        } else if largestY > max.y {
+            limitedV.y =  (max.y - largestY)
+        }
+
+        self.center?.p = initP + v + limitedV
+        self.anchors.a = initA + v + limitedV
+        self.anchors.b = initB + v + limitedV
+        self.anchors.c = initC + v + limitedV
     }
 }
