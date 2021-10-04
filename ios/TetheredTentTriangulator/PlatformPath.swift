022aa9f7c71705bb20ed3d288246e2339554d8c3
Karl Arthur <karl.crispen.arthur@gmail.com>
2021-10-03 18:44:03 -0600

    feat: ios: Provide paths for Trilogy Tentsile setup

--- ios/TetheredTentTriangulator/PlatformPath.swift
+++ ios/TetheredTentTriangulator/PlatformPath.swift
@@ -17,6 +17,7 @@ private let TENTSILE_BASE_DUO: Float = 2.7
 private let TENTSILE_BASE_FLITE: Float = 2.7
 private let TENTSILE_BASE_T_MINI: Float = 2.7
 private let TENTSILE_BASE_UNA: Float = 1.6
+private let TENTSILE_BASE_TRILOGY: Float = TENTSILE_BASE_CONNECT
 private let TENTSILE_CENTER_HOLE_HYPOTENUSE: Float = 0.6
 private let TENTSILE_NOTCH_SCALE: Float = 0.5
 private let TENTSILE_HYPOTENUSE_CONNECT: Float = 4.0
@@ -29,6 +30,7 @@ private let TENTSILE_HYPOTENUSE_TRILLIUM_XL: Float = 6.0
 private let TENTSILE_HYPOTENUSE_VISTA: Float = 4.1
 private let TENTSILE_HYPOTENUSE_UNA: Float = 2.9
 private let TENTSILE_HYPOTENUSE_UNIVERSE: Float = 4.4
+private let TENTSILE_HYPOTENUSE_TRILOGY: Float = TENTSILE_HYPOTENUSE_CONNECT
 
 struct PlatformPath {
     lazy var connect: [[Coordinate]] = getTenstsileIsosceles(hypotenuse: TENTSILE_HYPOTENUSE_CONNECT, base: TENTSILE_BASE_CONNECT)
@@ -38,7 +40,7 @@ struct PlatformPath {
     lazy var t_mini: [[Coordinate]] = getTenstsileIsosceles(hypotenuse: TENTSILE_HYPOTENUSE_T_MINI, base: TENTSILE_BASE_T_MINI)
     lazy var trillium: [[Coordinate]] = getTentsileEquilateral(longest: TENTSILE_HYPOTENUSE_TRILLIUM)
     lazy var trillium_xl: [[Coordinate]] = getTentsileEquilateral(longest: TENTSILE_HYPOTENUSE_TRILLIUM_XL)
-    lazy var trilogy: [[Coordinate]] = [[Coordinate]]()
+    lazy var trilogy: [[Coordinate]] = getTentsileTrilogy(hypotenuse: TENTSILE_HYPOTENUSE_TRILOGY, base: TENTSILE_BASE_TRILOGY)
     lazy var una: [[Coordinate]] = getTenstsileIsosceles(hypotenuse: TENTSILE_HYPOTENUSE_UNA, base: TENTSILE_BASE_UNA)
     lazy var universe: [[Coordinate]] = getTentsileEquilateral(longest: TENTSILE_HYPOTENUSE_UNIVERSE)
     lazy var vista: [[Coordinate]] = getTentsileEquilateral(longest: TENTSILE_HYPOTENUSE_VISTA)
@@ -110,4 +112,23 @@ private func getIsoscelesMeasurements(hypotenuse: Float, base: Float) -> [Float]
     let barb = notch * 2
     let point = sqrt(hypotenuse * hypotenuse - 3 * notch * notch) - notch
     return [point, barb, notch]
+}
+
+private func getTentsileTrilogy(hypotenuse: Float, base: Float) -> [[Coordinate]] {
+    let basePlatform = getTenstsileIsosceles(hypotenuse: hypotenuse, base: base)
+    let shiftedPlatform = basePlatform.translated(by: Coordinate(x: base * MATH_DIVIDE_BY_SQRT_THREE, y: 0))
+    let rotatedPlatformCCW = shiftedPlatform.rotated(by: MATH_TWO_THIRDS_PI)
+    let rotatedPlatformCW = shiftedPlatform.rotated(by: -MATH_TWO_THIRDS_PI)
+
+    var allShapes = [[Coordinate]]()
+    for shape in shiftedPlatform {
+        allShapes.append(shape)
+    }
+    for shape in rotatedPlatformCCW {
+        allShapes.append(shape)
+    }
+    for shape in rotatedPlatformCW {
+        allShapes.append(shape)
+    }
+    return allShapes
 }
